9b4835e63de3432063f8d21bec12ef2e67731af6
Nya Garcia <nyablk97@gmail.com>
2019-10-09 14:06:30 +0200

    feat(hotel-edit.container): implement container logic

--- src/pods/hotel-edit/hotel-edit.container.tsx
+++ src/pods/hotel-edit/hotel-edit.container.tsx
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const HotelEditContainer = () => {};
+import { CITIES } from 'common/constants/main.constants';
+import { HotelEdit } from './hotel-edit.component';
+import { ValidationErrors } from 'final-form';
+import { validateForm } from './hotel-edit.validation';
+
+export const HotelEditContainer = () => {
+  const [hotel, setHotel] = useState();
+
+  const validate = (values: any): Promise<ValidationErrors> => {
+    return validateForm(values);
+  };
+
+  const submit = (values: any) => {
+    console.log('Successfuly edited hotel', values);
+  };
+
+  return <HotelEdit validate={validate} submit={submit} hotel={hotel} cities={CITIES} />;
+};
