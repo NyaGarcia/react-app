eae8133537280bfed91687d77587dcf64ff4dc08
Nya Garcia <nyablk97@gmail.com>
2019-10-09 14:05:59 +0200

    feat(hotel-edit.props): add validate and submit props

--- src/pods/hotel-edit/hotel-edit.props.interface.tsx
+++ src/pods/hotel-edit/hotel-edit.props.interface.tsx
@@ -1,7 +1,10 @@
 import { City } from 'common/interfaces/city.interface';
-import { Hotel } from 'pods/hotel-collection/hotel-collection.interface';
+import { Hotel } from 'common/interfaces/hotel.interface';
+import { ValidationErrors } from 'final-form';
 
 export interface Props {
   hotel: Hotel;
   cities: Array<City>;
+  validate: (values: any) => Promise<ValidationErrors>;
+  submit: (values: any) => void;
 }
