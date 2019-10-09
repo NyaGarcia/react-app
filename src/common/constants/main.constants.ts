fa860a4a382e744ec95428117da72b173abcf743
Nya Garcia <nyablk97@gmail.com>
2019-10-09 14:04:08 +0200

    feat(main.constants): add CITIES constant

--- src/common/constants/main.constants.ts
+++ src/common/constants/main.constants.ts
@@ -1,4 +1,5 @@
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import { City } from 'common/interfaces/city.interface';
 import ErrorIcon from '@material-ui/icons/Error';
 import InfoIcon from '@material-ui/icons/Info';
 import WarningIcon from '@material-ui/icons/Warning';
@@ -12,3 +13,5 @@ export const VARIANTS = {
 
 export const BASE_URL = 'http://localhost:3000';
 export const HOTELS_URL = `${BASE_URL}/api/hotels`;
+
+export const CITIES: Array<City> = [{ name: 'Madrid' }, { name: 'Barcelona' }, { name: 'Málaga' }];
