0bb7893b0a350d7f3ed29c2a6d49e328a45683c3
Nya Garcia <nyablk97@gmail.com>
2019-10-09 14:09:02 +0200

    feat(hotel-collection.props): add editHotel prop

--- src/pods/hotel-collection/hotel-collection.props.ts
+++ src/pods/hotel-collection/hotel-collection.props.ts
@@ -1,6 +1,7 @@
-import { Hotel } from './hotel-collection.interface';
+import { Hotel } from '../../common/interfaces/hotel.interface';
 
 export interface Props {
   hotelCollection: Array<Hotel>;
   loading: boolean;
+  editHotel: (id: string) => void;
 }
